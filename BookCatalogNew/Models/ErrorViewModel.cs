d93d8de1998805e09e954edc62525a8a6bfe13a4
Shruti <shrutijmhatre@gmail.com>
2020-09-17 20:58:21 +0530

    Add project files.

---
+++ BookCatalogNew/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookCatalogNew.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
