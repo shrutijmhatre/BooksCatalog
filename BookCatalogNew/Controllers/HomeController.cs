5b8ca9360f045b850329c88ef403be6680b5e709
Shruti <shrutijmhatre@gmail.com>
2020-09-17 21:07:00 +0530

    Committing

--- BookCatalogNew/Controllers/HomeController.cs
+++ BookCatalogNew/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace BookCatalogNew.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Description()
         {
             return View();
         }
